af889b9e349065115463539cece8cd158a5d05ef
renuka-sn <renukanarasareddy8@gmail.com>
2022-04-21 23:09:22 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/img/hero-pattern.svg')",
+        "footer-texture": "url('/img/footer-texture.png')",
+      },
+    },
+  },
+};
+
+module.exports = {
+  theme: {
+    extend: {
+      translate: {
+        4.25: "17rem",
+      },
+    },
+  },
+};
